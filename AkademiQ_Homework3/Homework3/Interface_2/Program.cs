3aa24f43faeb18e8c4b36f6be634f9784d7c59e9
AysenurDarama <aysenurdrm123@gmail.com>
2025-03-08 00:42:15 +0300

    new commit

--- AkademiQ_Homework3/Homework3/Interface_2/Program.cs
+++ AkademiQ_Homework3/Homework3/Interface_2/Program.cs
@@ -1,11 +1,11 @@
-﻿/*  ABSTRACT CLASS                                              INTERFACE
+﻿/*  ABSTRACT CLASS                                                INTERFACE
    
- 1- Hem gövdeli hem de soyut metotlar olabilir.	                1- Sadece imza.
- 2-	Field tanımlanabilir.	                                    2- Field tanımlanamaz.
- 3-	Bir sınıf yalnızca bir tane abstract sınıftan türeyebilir.	3- Bir sınıf birden fazla interface uygulayabilir.
- 4-	Doğrudan nesne oluşturulamaz.	                            4- Doğrudan nesne oluşturulamaz.
- 5-	Ortak özellikleri paylaşmak için kullanılır.	            5- Farklı sınıfların aynı metotları kullanmasını
-                                                                  zorlamak için kullanılır.*/
+ 1- Hem gövdeli hem de soyut metotlar olabilir.	                  1- Sadece imza.
+ 2-	Field tanımlanabilir.	                                      2- Field tanımlanamaz.
+ 3-	Bir sınıf yalnızca bir tane abstract sınıftan türeyebilir.	  3- Bir sınıf birden fazla interface uygulayabilir.
+ 4-	Doğrudan nesne oluşturulamaz.	                              4- Doğrudan nesne oluşturulamaz.
+ 5-	Ortak özellikleri paylaşmak için kullanılır.	              5- Farklı sınıfların aynı metotları kullanmasını
+                                                                    zorlamak için kullanılır.*/
 using Interface_2;
 
 IDrawable shape_1 = new Circle(2);
